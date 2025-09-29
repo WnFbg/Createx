72ee8616f2c711a01ba81c3012bb3705605841ce
WnFbg <wn0fbg@gmail.com>
2025-09-29 18:58:16 +0300

    Commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function () {
-//   var mixed = mixitup(".directions__list");
+  var mixed = mixitup(".directions__list");
+
+  $(".directions__filter-btn").on("click", function () {
+    $(".directions__filter-btn").removeClass("directions__filter-btn--active");
+    $(this).addClass("directions__filter-btn--active");
+  });
+
+  $('.team__slider').slick({
+    arrows: false,
+    slidesToShow: 4
+  })
 });
